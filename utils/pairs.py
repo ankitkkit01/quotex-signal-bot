75b0d2e6176355b058658c7a115866309b83eabd
ankitkkit01 <akhimanshya@gmail.com>
2025-07-01 02:13:32 +0530

    Create pairs.py

---
+++ utils/pairs.py
@@ -0,0 +1,20 @@
+# utils/pairs.py
+
+all_pairs = [
+    # Major Currencies
+    "EUR/USD", "GBP/USD", "USD/JPY", "AUD/USD", "USD/CHF", "NZD/USD", "USD/CAD",
+    "EUR/JPY", "GBP/JPY", "AUD/JPY", "EUR/GBP", "EUR/CAD", "USD/INR", "USD/PKR",
+
+    # Minor & Exotic Currencies
+    "USD/IDR", "USD/MXN", "USD/BRL", "USD/ZAR", "USD/TRY", "USD/COP", "AUD/NZD",
+    "AUD/CHF", "NZD/CHF", "NZD/CAD", "GBP/AUD", "EUR/CHF",
+
+    # Cryptocurrencies
+    "BTC/USD", "ETH/USD", "XRP", "LTC", "DOGE", "SOL", "SHIBA", "TRUMP", "TON",
+
+    # Commodities
+    "GOLD", "SILVER", "CRUDEOIL",
+
+    # Bonus Stock/Index Pairs (if needed)
+    # "AAPL", "TSLA", "AMZN", "US100", "SP500", etc. (you can add OTC later)
+]
