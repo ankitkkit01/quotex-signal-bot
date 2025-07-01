6eaa3bcfcd54de2064a9cbba59bc2da3f8dc653a
ankitkkit01 <akhimanshya@gmail.com>
2025-07-01 13:15:39 +0530

    Create setup.py

---
+++ quotexpy/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='quotexpy',
+    version='1.0.0',
+    description='Quotex API custom integration module',
+    author='Ankit Singh',
+    author_email='ankit@example.com',
+    packages=find_packages(),
+    install_requires=[],
+)
